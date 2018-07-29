15a8295284ddeea4f3c6e20f1f189ec83f90e400
nik frank <nikbfrank@gmail.com>
2018-07-29 20:45:21 +0300

    dealer boilerplate

---
+++ src/Dealer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Dealer.css';
+
+class Dealer extends Component {
+  state = {}
+
+  render(){
+    return (
+      <div>
+        Coming Soon...
+      </div>
+    );
+  }
+}
+
+export default Dealer;
